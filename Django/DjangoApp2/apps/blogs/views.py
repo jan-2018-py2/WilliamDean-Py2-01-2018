30bba88b90fb3c6c2049fd0a5f918f9a002ed19a
williamnewing <william.dean.newing@gmail.com>
2018-01-13 16:29:30 -0800

    sandboxing blogs

--- Django/DjangoApp2/apps/blogs/views.py
+++ Django/DjangoApp2/apps/blogs/views.py
@@ -1,16 +1,29 @@
 from __future__ import unicode_literals
 
-from django.shortcuts import HttpResponse, redirect
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 def index(request):
-    return HttpResponse("placeholder to later display all the list of blogs")
+    context = {
+        "email" : "blog@gmail.com",
+        "name" : "mike"
+    }   
+    return render(request, "blogs/index.html", context)
 
 def new(request):
     return HttpResponse("placeholder to display a new form to create a new blog")
 
 def create(request):
-    return redirect('/')
+    if request.method == "POST":
+        print "*" * 50
+        print request.POST
+    print request.POST['name']
+    print request.POST['desc']
+    request.session['name'] = "test" 
+        print "*" * 50
+        return redirect("/")
+    else:
+        return redirect("/")
 
 def show(request, blog_id):
     print blog_id
