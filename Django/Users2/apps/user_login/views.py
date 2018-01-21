0de22f1b96be11d4d6b5094add5313a47e9ffdfa
williamnewing <william.dean.newing@gmail.com>
2018-01-20 17:44:32 -0800

    DojoNinja2HW

---
+++ Django/Users2/apps/user_login/views.py
@@ -0,0 +1,6 @@
+from __future__ import unicode_literals
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Go make shell queries")
